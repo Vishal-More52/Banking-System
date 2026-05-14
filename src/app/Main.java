95bcae23f43a9f5e40da9e66b7460d8103922b3b
Vishal More <vishalmore4821@gmail.com>
2026-05-14 10:16:33 +0530

    Adding repeted boiler code in main

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,17 +1,32 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
         System.out.println("Welcome to Console Bank");
-        System.out.println("""
-                1) Open Account
-                2) Deposit
-                3) Withdraw
-                4) Transfer
-                5) Account Statement
-                6) List Accounts
-                7) Search Accounts By Customer Name
-                0) Exit                    
-                """);
+        while (running) {
+            System.out.println("""
+                    1) Open Account
+                    2) Deposit
+                    3) Withdraw
+                    4) Transfer
+                    5) Account Statement
+                    6) List Accounts
+                    7) Search Accounts By Customer Name
+                    0) Exit                    
+                    """);
+
+            System.out.println("CHOOSE: ");
+            String choice = scanner.nextLine().trim();
+            System.out.println("Choice : " + choice);
+
+            switch (choice){
+                case  "0" -> running = false;
+            }
+        }
     }
 }
